7872f30089a8cbbde61741849788e79dffdea13c
Practice <meteranb@unal.edu.co>
2025-04-08 07:58:48 -0500

    Adding Products and Descriptions in each page

--- pages/Rubores.py
+++ pages/Rubores.py
@@ -1,6 +1,14 @@
 from functions import main_page
 import streamlit as st
+import pandas as pd
 
-st.title("Rubores")
+df = pd.read_excel("Inventario.xlsx", sheet_name = "Rubores")
+
+for index, row in df.iterrows():
+    st.header(row["Nombre del Producto"])
+    st.image("images/" + row["Imagen"])
+    st.write(row["Descripcion"])
+    st.info(f"Precio: $ {row["Valor"]}")
+    st.info(f"Disponibilidad: {row["Disponible"]}")
 
 main_page()
